4ff277e9fe00da7ca3a611724e633931e7129a13
blackflame576 <pws15548@gmail.com>
2024-03-09 08:57:19 +0300

    Refactoring

---
+++ src/LogClient.cpp
@@ -0,0 +1,76 @@
+#include "include/LogClient.hpp"
+
+int LogClient::Client::POST() {
+    curl = curl_easy_init();
+    if (curl)
+    {
+        curl_easy_setopt(curl, CURLOPT_URL, LOGSERVER_URL);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+        string dataStr = data.toStyledString();
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, dataStr.c_str());
+        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, curl_slist_append(NULL, "Content-Type: application/json"));
+        curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+        curl_easy_setopt(curl, CURLOPT_FILETIME, 1L);
+        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+        curl_easy_setopt(curl, CURLOPT_NOSIGNAL, 1);
+
+        res = curl_easy_perform(curl);
+
+        if (res != CURLE_OK) {
+            cerr << "POST request failed: " << curl_easy_strerror(res) << endl;
+        }
+
+        curl_easy_cleanup(curl);
+        return res;
+    }
+    else {
+
+        return 1;
+    }
+}
+
+int LogClient::Client::GET() {
+    curl = curl_easy_init();
+    if (curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, GET_URL.c_str());
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, curl_slist_append(NULL, "Content-Type: application/json"));
+        curl_easy_setopt(curl, CURLOPT_FILETIME, 1L);
+        curl_easy_setopt(curl, CURLOPT_NOSIGNAL, 1);
+        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+        res = curl_easy_perform(curl);
+        if (res != CURLE_OK) {
+            cerr << "GET request failed: " << curl_easy_strerror(res) << endl;
+        }
+
+        curl_easy_cleanup(curl);
+        return res;
+    }
+    else
+    {
+        return 1;
+    }
+}
+
+int LogClient::Client::sendRequest(string type)
+{
+    try {
+        cout << "Hello" << endl;
+        (client.*(Methods[type]))();
+    }
+    catch (exception &error) {
+        cout << error.what() << '\n';
+    }
+    return 0;
+}
+
+int main() {
+    LogClient::client.data["title"] = "Hello, world!";
+    LogClient::client.sendRequest("POST");
+//    LogClient::
+    LogClient::client.sendRequest("GET");
+}
---
+++ src/include/LogClient.hpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include "curl/curl.h"
+#include "json/json.h"
+#include <map>
+#include <exception>
+
+#define LOGSERVER_URL "http://localhost:8000/api/add_log"
+
+using namespace std;
+using namespace Json;
+
+namespace LogClient
+{
+    size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp) {
+        ((string *) userp)->append((char *) contents, size * nmemb);
+        return size * nmemb;
+    }
+
+    class Client {
+    public:
+        CURL *curl;
+        CURLcode res;
+        // string type
+        string readBuffer;
+        string GET_URL = "logserver.alwaysdata.net/api/logs/wake_up_tests";
+        Value data;
+
+
+        int sendRequest(string type);
+
+        int POST();
+
+        int GET();
+
+        Client() {
+//            curl_global_init(CURL_GLOBAL_DEFAULT);
+            curl = curl_easy_init();
+        }
+
+    private:
+        using funct_t = int (Client::*)(void);
+        map<string, funct_t> Methods = {
+                {"POST", &Client::POST},
+                {"GET",  &Client::GET}};
+    };
+
+    Client client;
+}
